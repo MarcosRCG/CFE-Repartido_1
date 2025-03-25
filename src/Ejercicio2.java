c099f6cf8f610339bec8f59dc1796c53451024f5
MarcosRCG <marcosrafael951@gmail.com>
2025-03-25 13:04:53 -0300

    First commit

---
+++ src/Ejercicio2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número: ");
+        int num1 = leer.nextInt();
+        System.out.println("Ingrese el segundo número: ");
+        int num2 = leer.nextInt();
+
+        System.out.println("Resultado de la suma: " + (num1 + num2));
+    }
+}
